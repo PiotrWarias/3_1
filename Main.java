0a631b0eab81f3b6620832241f1f64c46fd971e0
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-04-12 11:13:57 +0000

    3_2 commit działający nonsstop

--- Main.java
+++ Main.java
@@ -8,29 +8,41 @@ public static void main(String[] args) {
 try {
 Service1 service = new Service1();
 Service1 metoda = new Service1();
-System.out.println("Wybierz co chcesz zrobic\n1.Dopisac studenta\n2.Wyswietlic studentow\n3.Wyjdz");
+int k=0;
+  while(true)
+  {
+System.out.println("\nWybierz co chcesz zrobic\n1.Dopisac studenta\n2.Wyswietlic studentow\n3.Wyjdz\n");
 Scanner scanner = new Scanner(System.in);
 int opcja = scanner.nextInt();
 String imie = scanner.nextLine();
 int wiek;
+String data;
 switch(opcja)
 {
 case 1:
-System.out.println("Wpisz imie");
-imie = scanner.nextLine();
-System.out.println("Wpisz wiek");
-wiek = scanner.nextInt();
-System.out.println("Dodano studenta");
-metoda.addStudent(new Student(imie, wiek));
+  System.out.println("Wpisz imie");
+  imie = scanner.nextLine();
+  System.out.println("Wpisz wiek");
+  wiek = scanner.nextInt();
+  System.out.println("Wpisz rok");
+  scanner.nextLine();
+  data = scanner.nextLine();
+  System.out.println("Dodano studenta");
+  metoda.addStudent(new Student(imie, wiek, data));
 break;
+    
 case 2:
-var students = service.getStudents();
-for(Student current : students) {
-System.out.println(current.ToString());
-}
+  var students = service.getStudents();
+  for(Student current : students) {
+  System.out.println(current.ToString());
+  }
 break;
+
 case 3:
+k=1;
 break;
+}
+    if (k==1)break;
 }
 }
 catch (IOException e) {
--- Student.java
+++ Student.java
@@ -1,24 +1,23 @@
 public class Student {
-
-  private String Name;
-  private int Age;
-
-  public Student(String name, int age) {
-    Name = name;
-    Age = age;
-  }
-
-  public String GetName() {return Name;}
-  public int GetAge() {return Age;}
-
-  public String ToString() {
-    return Name + " " + Integer.toString(Age);
-  }
-
-  public static Student Parse(String str) {
-    String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
-  }
+private String Name;
+private int Age;
+private String Data;
+  
+public Student(String name, int age, String data) {
+Name = name;
+Age = age;
+Data = data;
+}
+public String GetName() {return Name;}
+public int GetAge() {return Age;}
+public String GetData(){return Data;}
+public String ToString() {
+return Name + " " + Integer.toString(Age) + " " + Data;
+}
+public static Student Parse(String str) {
+String[] data = str.split(" ");
+if(data.length != 3)
+return new Student("Parse Error",-1,"parse Error");
+return new Student(data[0], Integer.parseInt(data[1]),data[2]);
+}
 }
